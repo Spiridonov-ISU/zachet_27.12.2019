a6f201d493ba109c740e87bc6a60d8c626579750
Spiridonov-ISU <56498495+Spiridonov-ISU@users.noreply.github.com>
2019-12-27 13:53:35 +0800

    Update perevorot_matrizi.cpp

--- perevorot_matrizi.cpp
+++ perevorot_matrizi.cpp
@@ -1,46 +1,46 @@
-//#include<iostream>
-//#include<fstream>
-//#include<conio.h>
-//#include<locale.h>
-//
-//using namespace std;
-//
-//int main(){
-//	setlocale(LC_ALL,"Russian");
-//	ifstream in("arr.txt");
-//	if(!in.is_open()){
-//		cout<<"Error!"<<endl;
-//		return -1;
-//	}
-//
-//	const int n=10;
-//	int arr[n][n];
-//	int gr=0;
-//
-//	do{
-//		cout<<"Ââåäèòå íà ñêîëüêî ãððàäóñîâ õîòèòå ïîâåðíóòü ìàññèâ(ëèáî 45 ëèáî 90): "; cin>>gr; cout<<endl;
-//	}
-//	while(gr!=45 && gr!=90);
-//		for(int i=0; i<n; i++){
-//			cout<<endl;
-//			for(int j=0; j<n; j++){
-//				in>>arr[i][j];
-//				cout<<arr[i][j]<<" ";
-//			}
-//		}
-//	cout<<endl<<endl<<"Ïåðåâåðíóòàÿ ìàòðèöà íà "<<gr<<" ãðàäóñîâ:"<<endl;
-//	if(gr==45)
-//		for(int j=0; j<n; j++){
-//			cout<<endl;
-//			for(int i=0; i<n; i++)
-//				cout<<arr[i][j]<<" ";
-//		}
-//	else 
-//		for(int i=9; i>-1; i--){
-//			cout<<endl;
-//			for(int j=9; j>-1; j--)
-//				cout<<arr[i][j]<<" ";
-//		}
-//	_getch();
-//	return 0;
-//}
+#include<iostream>
+#include<fstream>
+#include<conio.h>
+#include<locale.h>
+
+using namespace std;
+
+int main(){
+	setlocale(LC_ALL,"Russian");
+	ifstream in("arr.txt");
+	if(!in.is_open()){
+		cout<<"Error!"<<endl;
+		return -1;
+	}
+
+	const int n=10;
+	int arr[n][n];
+	int gr=0;
+
+	do{
+		cout<<"Ã‚Ã¢Ã¥Ã¤Ã¨Ã²Ã¥ Ã­Ã  Ã±ÃªÃ®Ã«Ã¼ÃªÃ® Ã£Ã°Ã°Ã Ã¤Ã³Ã±Ã®Ã¢ ÃµÃ®Ã²Ã¨Ã²Ã¥ Ã¯Ã®Ã¢Ã¥Ã°Ã­Ã³Ã²Ã¼ Ã¬Ã Ã±Ã±Ã¨Ã¢(Ã«Ã¨Ã¡Ã® 45 Ã«Ã¨Ã¡Ã® 90): "; cin>>gr; cout<<endl;
+	}
+	while(gr!=45 && gr!=90);
+		for(int i=0; i<n; i++){
+			cout<<endl;
+			for(int j=0; j<n; j++){
+				in>>arr[i][j];
+				cout<<arr[i][j]<<" ";
+			}
+		}
+	cout<<endl<<endl<<"ÃÃ¥Ã°Ã¥Ã¢Ã¥Ã°Ã­Ã³Ã²Ã Ã¿ Ã¬Ã Ã²Ã°Ã¨Ã¶Ã  Ã­Ã  "<<gr<<" Ã£Ã°Ã Ã¤Ã³Ã±Ã®Ã¢:"<<endl;
+	if(gr==45)
+		for(int j=0; j<n; j++){
+			cout<<endl;
+			for(int i=0; i<n; i++)
+				cout<<arr[i][j]<<" ";
+		}
+	else 
+		for(int i=9; i>-1; i--){
+			cout<<endl;
+			for(int j=9; j>-1; j--)
+				cout<<arr[i][j]<<" ";
+		}
+	_getch();
+	return 0;
+}
