77a43e258b2b89d0496b2c6b46d74ba3a7799cd4
Spiridonov-ISU <56498495+Spiridonov-ISU@users.noreply.github.com>
2019-12-27 13:51:41 +0800

    Update factorial.cpp

--- factorial.cpp
+++ factorial.cpp
@@ -1,23 +1,23 @@
-//#include<iostream>
-//#include<fstream>
-//#include<conio.h>
-//#include<locale.h>
-//
-//using namespace std;
-//
-//int main(){
-//	setlocale(LC_ALL,"Russian");
-//	ofstream out("factorial.txt");
-//
-//	double fact=1.0;
-//	const int count_fact=100;
-//	for(int i=1; i<count_fact+1; i++){
-//		for(double j=2.0; j<i+1; j++)
-//			fact*=j;
-//		out<<fact<<endl;
-//		fact=1;
-//	}
-//	cout<<"Ïðîãðàììà âûïîëíåíà!";
-//	_getch();
-//	return 0;
-//}
+#include<iostream>
+#include<fstream>
+#include<conio.h>
+#include<locale.h>
+
+using namespace std;
+
+int main(){
+	setlocale(LC_ALL,"Russian");
+	ofstream out("factorial.txt");
+
+	double fact=1.0;
+	const int count_fact=100;
+	for(int i=1; i<count_fact+1; i++){
+		for(double j=2.0; j<i+1; j++)
+			fact*=j;
+		out<<fact<<endl;
+		fact=1;
+	}
+	cout<<"ÃÃ°Ã®Ã£Ã°Ã Ã¬Ã¬Ã  Ã¢Ã»Ã¯Ã®Ã«Ã­Ã¥Ã­Ã !";
+	_getch();
+	return 0;
+}
