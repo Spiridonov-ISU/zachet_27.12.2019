0c8860e0776ea9abe9823632988703f5f996bdb7
Spiridonov-ISU <56498495+Spiridonov-ISU@users.noreply.github.com>
2019-12-27 13:52:19 +0800

    Add files via upload

---
+++ perevorot_matrizi.cpp
@@ -0,0 +1,46 @@
+//#include<iostream>
+//#include<fstream>
+//#include<conio.h>
+//#include<locale.h>
+//
+//using namespace std;
+//
+//int main(){
+//	setlocale(LC_ALL,"Russian");
+//	ifstream in("arr.txt");
+//	if(!in.is_open()){
+//		cout<<"Error!"<<endl;
+//		return -1;
+//	}
+//
+//	const int n=10;
+//	int arr[n][n];
+//	int gr=0;
+//
+//	do{
+//		cout<<"¬ведите на сколько гррадусов хотите повернуть массив(либо 45 либо 90): "; cin>>gr; cout<<endl;
+//	}
+//	while(gr!=45 && gr!=90);
+//		for(int i=0; i<n; i++){
+//			cout<<endl;
+//			for(int j=0; j<n; j++){
+//				in>>arr[i][j];
+//				cout<<arr[i][j]<<" ";
+//			}
+//		}
+//	cout<<endl<<endl<<"ѕеревернута€ матрица на "<<gr<<" градусов:"<<endl;
+//	if(gr==45)
+//		for(int j=0; j<n; j++){
+//			cout<<endl;
+//			for(int i=0; i<n; i++)
+//				cout<<arr[i][j]<<" ";
+//		}
+//	else 
+//		for(int i=9; i>-1; i--){
+//			cout<<endl;
+//			for(int j=9; j>-1; j--)
+//				cout<<arr[i][j]<<" ";
+//		}
+//	_getch();
+//	return 0;
+//}
